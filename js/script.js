7ffce02bd54b74ca4fd3ef0ac5bf627b3746e415
Keila <keiladaianefernandes24@gmail.com>
2020-02-18 16:47:18 -0300

    Iniciando modo dark

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+let buttonDarkmode = document.querySelector('.lamp')
+let logo = document.querySelector('.logo')
+let body = document.querySelector('body')
 let buttonWantToHelp = document.querySelector('header button')
 let form = document.querySelector('.form')
+
 
 buttonWantToHelp.addEventListener('click', () => {
     form.classList.toggle('hide')
 })
+
+buttonDarkmode.addEventListener('click', () => {
+    body.classList.toggle('dark-mode')
+})
+
