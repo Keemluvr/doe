b1e887ca459c79e22f4372a7fdd9cfa24b491c24
Keila <keiladaianefernandes24@gmail.com>
2020-02-19 21:27:54 -0300

    Modo dark adicionado sem utiliza o storage ainda

--- public/js/script.js
+++ public/js/script.js
@@ -5,16 +5,25 @@ let buttonWantToHelp = document.querySelector("header button");
 let form = document.querySelector(".form");
 let formButton = document.querySelector(".form button")
 
+// Abre o form para inscrever doador
 buttonWantToHelp.addEventListener("click", () => {
   form.classList.toggle("hide");
 });
 
+
+// Recarega ao enviar um novo doador
 formButton.addEventListener("click", () => {
   window.location.reload()
 })
 
-// buttonDarkmode.addEventListener('click', () => {
-//     body.classList.toggle('dark-mode')
-// })
+
+// Botão de escurecer a tela
+buttonDarkmode.addEventListener('click', () => {
+    body.classList.toggle('dark-mode')
+    if (body.classList.contains('dark-mode')) 
+      logo.setAttribute('src', "assets/img/logo-light.png")
+    else
+      logo.setAttribute('src', "assets/img/logo.png")
+})
 
 
